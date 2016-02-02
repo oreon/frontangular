4545c7e1aff67b3f89207cbf6248342ddc379072
singhj2014 <singhjess@gmail.com>
2016-02-02 09:37:52 -0500

    added smart table

--- src/client/app/employee/controllers/employeesDirective.js
+++ src/client/app/employee/controllers/employeesDirective.js
@@ -5,6 +5,34 @@ angular.module('app.employee').directive('employeesDirective', function () {
             employees: '=',
             tableParams: '='
         },
-        templateUrl: 'app/employee/views/employeesTable.html'
+        templateUrl: 'app/employee/views/employeesTable.html',
+        
+        link: function (scope, elem, attrs) {
+            var nameList = ['Pierre', 'Pol', 'Jacques', 'Robert', 'Elisa'], familyName = ['Dupont', 'Germain', 'Delcourt', 'bjip', 'Menez'];
+            function createRandomItem() {
+                var
+                    firstName = nameList[Math.floor(Math.random() * 4)],
+                    lastName = familyName[Math.floor(Math.random() * 4)],
+                    age = Math.floor(Math.random() * 100),
+                    email = firstName + lastName + '@@whatever.com',
+                    balance = Math.random() * 3000;
+
+                return {
+                    firstName: firstName,
+                    lastName: lastName,
+                    age: age,
+                    email: email,
+                    balance: balance
+                };
+            }
+
+            scope.employees = [];
+            for (var j = 0; j < 10; j++) {
+                console.log("adding employee" + j)
+                scope.employees.push(createRandomItem());
+            }
+            ///scope.dataset = [].concat(scope.rowCollection);
+        }
+        
     };
 });
